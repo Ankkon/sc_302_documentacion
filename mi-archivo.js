4d2edfe70a32ea2e20f00852be8e20847a5324bb
GuillermoAndresDelgadoCercone <113642438+Ankkon@users.noreply.github.com>
2025-02-06 20:29:13 -0600

    Update mi-archivo.js

--- mi-archivo.js
+++ mi-archivo.js
@@ -5,3 +5,5 @@ console.log(‘hola mundo’)
 //Paso 1 git add
 //Paso 2 git commit -m "Mensaje de la modificación"
 //Paso 3 git push (Para sincronizar el cambio al GitHub)
+
+//Comentario para un segundo push mediante GitHub Desktop.
