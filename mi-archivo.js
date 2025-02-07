d7650c8f8ee903166647c0fca5118d56e975cbe5
GuillermoAndresDelgadoCercone <113642438+Ankkon@users.noreply.github.com>
2025-02-06 20:27:52 -0600

    Update mi-archivo.js
    Update desde GitHub Desktop

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,7 @@
 console.log(‘hola mundo’)
 
 //Cuando ya no se va a realizar mas cambios con el comando "add" se pasa el archivo a la stating area
+
+//Paso 1 git add
+//Paso 2 git commit -m "Mensaje de la modificación"
+//Paso 3 git push (Para sincronizar el cambio al GitHub)
